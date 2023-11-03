22852c8f4d3f1c3182b13c6bd5abb42bf465f542
Evans Murunga <91890645+Fffbbm@users.noreply.github.com>
2023-11-03 14:34:28 +0300

    updated

--- src/assets/component/Home.tsx
+++ src/assets/component/Home.tsx
@@ -1,12 +1,16 @@
+
 import React, { useState } from 'react';
 import Login from './Login';
-import Registration from "./Registration.tsx";
+import Registration from "./Registration";
 import './stylesheeet/home.css'
+import Ticket from "./dashboard/Ticket";
 
 const Home: React.FC = () => {
 
     const [showLoginForm, setShowLoginForm] = useState(false);
     const [showRegisterForm, setShowRegisterForm] = useState(false);
+    const [showTicketForm, setShowTicketForm] = useState(false);
+    const [showProfileDropdown, setShowProfileDropdown] = useState(false);
 
     const handleButtonClick = (formType: string) => {
         if (formType === 'login') {
@@ -15,7 +19,13 @@ const Home: React.FC = () => {
         } else if (formType === 'register') {
             setShowLoginForm(false);
             setShowRegisterForm(true);
+        } else if (formType === 'ticket') {
+            setShowTicketForm(false);
         }
+    };
+
+    const handleProfileClick = () => {
+        setShowProfileDropdown(!showProfileDropdown);
     };
 
     return (
@@ -23,21 +33,44 @@ const Home: React.FC = () => {
             <h1 className="home-title">Welcome to the Home Page</h1>
             <p className="home-content">This is the content of the home page.</p>
 
-            {showLoginForm ? (
-                <Login />
-            ) : (
-                <button className="home-button" onClick={() => handleButtonClick('login')}>
-                    Login
-                </button>
-            )}
+            <nav className="nav-container">
+                {showTicketForm ? (
+                    <Ticket />
+                ) : (
+                    <button className="ticket-button" onClick={() => handleButtonClick('ticket')} disabled={!showLoginForm && !showRegisterForm}>
+                        Ticket
+                    </button>
+                )}
 
-            {showRegisterForm ? (
-                <Registration />
-            ) : (
-                <button className="home-button" onClick={() => handleButtonClick('register')}>
-                    Register
-                </button>
-            )}
+                <div className="profile-dropdown">
+                    <button className="profile-button" onClick={handleProfileClick}>
+                        Profile
+                    </button>
+                    {showProfileDropdown && (
+                        <div className="dropdown-content">
+                            {showLoginForm ? (
+                                <div className="form-container">
+                                    <Login />
+                                </div>
+                            ) : (
+                                <button className="dropdown-button" onClick={() => handleButtonClick('login')}>
+                                    Login
+                                </button>
+                            )}
+
+                            {showRegisterForm ? (
+                                <div className="form-container">
+                                    <Registration />
+                                </div>
+                            ) : (
+                                <button className="dropdown-button" onClick={() => handleButtonClick('register')}>
+                                    Register
+                                </button>
+                            )}
+                        </div>
+                    )}
+                </div>
+            </nav>
         </div>
     );
 };
--- src/assets/component/Login.tsx
+++ src/assets/component/Login.tsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './stylesheeet/login.css';
+
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const checkAllInputs = () => {
-        return email !== '' && password !== ''
-    }
+        return email !== '' && password !== '';
+    };
+
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
 
@@ -19,61 +22,56 @@ const Login = () => {
             toast.success('Login successful!', {
                 position: toast.POSITION.TOP_RIGHT,
             });
-        }else {
-            if(email == ''){
+        } else {
+            if (email === '') {
                 toast.error('Please fill email input.', {
                     position: toast.POSITION.BOTTOM_RIGHT,
                 });
-            }else if(password == ''){
+            } else if (password === '') {
                 toast.error('Please fill password input', {
                     position: toast.POSITION.BOTTOM_RIGHT,
                 });
-            }else {
+            } else {
                 toast.error('Please fill in all the input fields.', {
                     position: toast.POSITION.BOTTOM_RIGHT,
                 });
             }
         }
-    }
+    };
 
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-6">
-                    <div className="card">
-                        <div className="card-body">
-                            <h3 className="card-title">Registration form</h3>
-                            <form onSubmit={handleSubmit}>
-                                <div className="form-group">
-                                    <label htmlFor="email">Email</label>
-                                    <input
-                                        type="email"
-                                        className="form-control"
-                                        id="email"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="password">Password</label>
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        id="password"
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                    />
-                                </div>
-                                <button type="submit" className="btn btn-primary">
-                                    Login
-                                </button>
-                            </form>
-                        </div>
+        <div className="login-container">
+            <div className="login-card">
+                <h3 className="card-title">Login</h3>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </div>
-                </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-primary">
+                        Login
+                    </button>
+                </form>
             </div>
             <ToastContainer />
         </div>
     );
 };
+
 export default Login;
